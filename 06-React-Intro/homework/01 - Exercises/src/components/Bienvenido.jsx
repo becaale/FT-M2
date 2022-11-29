0640164e7c46526d48e5ac6c7f82c56851455948
becaale <becaale@hotmail.com>
2022-11-29 10:41:58 +0200

    HomeWork Intro React Exercises 1/2 ok

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Alejandro Becagigi";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Titulo H1</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((skill) => {
+          return <li>{skill}</li>;
+        })}
+      </ul>
+      {console.log(alerts)}
+      <Botones alerts={alerts} />
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
