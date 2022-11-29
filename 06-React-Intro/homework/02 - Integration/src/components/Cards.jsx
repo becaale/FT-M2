a4a6a4b2aeeda49f26ed6237901041f33d740a69
becaale <becaale@hotmail.com>
2022-11-29 12:39:46 +0200

    HomeWork Integration ok

--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,6 +1,21 @@
-import Card from './Card';
+import Card from "./Card";
 
 export default function Cards(props) {
-   const { characters } = props;
-   return <div></div>;
+  console.log(props);
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map((character) => {
+        return (
+          <Card
+            name={character.name}
+            species={character.species}
+            gender={character.gender}
+            image={character.image}
+            onClose={() => window.alert("Emulamos que se cierra la card")}
+          />
+        );
+      })}
+    </div>
+  );
 }
