8961e84b739cd8b081da8f41ddd3a0b1c1e203a6
becaale <becaale@hotmail.com>
2022-12-06 18:16:28 +0200

    HomeWork React-Redux Exercise 1, 2 ok

--- 12-React-Redux/homework/01 - Exercises/src/redux/reducer/index.js
+++ 12-React-Redux/homework/01 - Exercises/src/redux/reducer/index.js
@@ -1,5 +1,23 @@
-// const initialState = {
-//   list: []
-// }
+import { ADD_PRODUCT, DELETE_PRODUCT } from "../actions/types";
 
-// export default rootReducer
+const initialState = {
+  list: [],
+};
+
+export default rootReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ADD_PRODUCT:
+      return { ...state, list: [...state.list, payload] };
+      break;
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        list: [...state.list.filter((element) => element.id !== payload)],
+      };
+      break;
+
+    default:
+      return state;
+      break;
+  }
+};
